3ba6965caebefff58693e70f6e3895c607427e00
Davi Mateus <davi.2020109493@unicap.br>
2023-10-15 11:52:10 -0300

    fix: add server host

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,8 @@ import app from '@/app'
 import '@/database'
 
 const serverPort = Number(process.env.SERVER_PORT)
+const serverHost = process.env.SERVER_HOST
 
-app.listen({ port: serverPort }, () => {
+app.listen({ port: serverPort, host: serverHost }, () => {
   console.log(`🚀 Server running at port ${serverPort}`)
 })
